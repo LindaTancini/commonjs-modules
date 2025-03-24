53f311a5216cb036f57272b8a675becfd78eceb8
LindaTancini <lindatancini95@gmail.com>
2025-03-24 14:56:09 +0100

    feat: exported function

--- names.js
+++ names.js
@@ -2,3 +2,6 @@ function createName(firstName, lastName) {
   return { firstName, lastName };
 }
 console.log(createName("Linda", "Tancini"));
+
+// ESPORTO LA FUNZIONE IN UN'ALTRO FOGLIO JS
+module.exports = createName;
