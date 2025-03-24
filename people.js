bcb9c469c0781d7787c6d7a9beeb46880e10fc1a
LindaTancini <lindatancini95@gmail.com>
2025-03-24 15:09:49 +0100

    feat: imported function

---
+++ people.js
@@ -0,0 +1,5 @@
+//  IMPORTO LE FUNZIONI DAGLI ALTRI FOGLI JS
+const createName = require("./names.js");
+const createHobbies = require("./hobbies.js");
+console.log(createName);
+console.log(createHobbies);
