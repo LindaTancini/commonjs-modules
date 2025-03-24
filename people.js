8bfd3de4598b8ad4cf91778cc4c90a91c16c5d3a
LindaTancini <lindatancini95@gmail.com>
2025-03-24 15:27:58 +0100

    feat: added the createPeople function with the other two exported functions inside

--- people.js
+++ people.js
@@ -3,3 +3,14 @@ const createName = require("./names.js");
 const createHobbies = require("./hobbies.js");
 console.log(createName);
 console.log(createHobbies);
+
+// FUNZIONE CHE PRENDE PARAMETRI DA ALTRE FUNZIONI
+function createPeople() {
+  const MyFullName = createName("Linda", "Tancini");
+  const myHobbies = createHobbies("Libri", "Videogames", "AnimeManga");
+  return {
+    MyFullName,
+    myHobbies,
+  };
+}
+console.log(createPeople());
