b6080462f5fec20968d260adf1f7f62f8279e353
LindaTancini <lindatancini95@gmail.com>
2025-03-24 14:56:54 +0100

    feat: exported function

--- hobbies.js
+++ hobbies.js
@@ -2,3 +2,6 @@ function createHobbies(hobbyOne, hobbyTwo, hobbyThree) {
   return [hobbyOne, hobbyTwo, hobbyThree];
 }
 console.log(createHobbies("Libri", "Videogames", "AnimeManga"));
+
+// ESPORTO LA FUNZIONE IN UN'ALTRO FOGLIO JS
+module.exports = createHobbies;
